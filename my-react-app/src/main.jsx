ce40bd07c989f4f6be5913dfb6c42b4894ad0c32
Tagan Ramsey <tbramsey5@gmail.com>
2025-11-08 22:23:20 -0600

    chatbot and algo

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+//import App from './App.jsx'
+import Chatbot from './components/chatbot.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div >
+      <Chatbot />
+    </div>
   </StrictMode>,
-)
+);
